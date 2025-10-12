2fae14c8cbb5f3443399e24d8835604a807eabcc
hhilmikk-collab <hhilmikk@gmail.com>
2025-10-12 12:36:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+var f = document.getElementById("frog");
+var f2 = document.getElementById("frog2");
+f.addEventListener("mousedown", Hide);
+f.addEventListener("mouseup", Show);
 
+window.addEventListener("load", function(){
+    f2.style.opacity = "0";
+})
+
+frog.addEventListener("contextmenu", e => e.preventDefault());
+frog.addEventListener("dragstart", e => e.preventDefault());
+frog.addEventListener("touchstart", e => e.stopPropagation());
+
+
+function Hide() {
+    //console.log("frog gone :o");
+    f.style.opacity = "0";
+    f2.style.opacity = "1";
+}
+
+function Show() {
+    //console.log("frog again :D");
+    f.style.opacity = "1";
+    f2.style.opacity = "0";
+}
